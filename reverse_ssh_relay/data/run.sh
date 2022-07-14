d700d13f2f0777543446d8725c44f1de5a56850e
Martijn <27301875+smarti@users.noreply.github.com>
2022-07-14 08:51:24 +0200

    Update run.sh

--- reverse_ssh_relay/data/run.sh
+++ reverse_ssh_relay/data/run.sh
@@ -17,4 +17,4 @@ sed -i "s/%%REMOTEUSER%%/$REMOTEUSER/g" sshtunnel.sh
 sed -i "s/%%REMOTEADDRESS%%/$REMOTEADDRESS/g" sshtunnel.sh
 sed -i "s/%%REMOTEPASSWORD%%/$REMOTEPASSWORD/g" sshtunnel.sh
 
-#./sshtunnel.sh
+./sshtunnel.sh
